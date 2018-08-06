9824a0456044ebcf98c3864ca58703e7bbfc346a
samirsumanjha <samirsumanjha@gmail.com>
2018-08-06 21:16:55 +0530

    Destini App

--- app/src/main/java/com/londonappbrewery/destini/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/destini/MainActivity.java
@@ -2,10 +2,20 @@ package com.londonappbrewery.destini;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     // TODO: Steps 4 & 8 - Declare member variables here:
+    TextView mStoryTextView;
+    Button   mButtonTop;
+    Button   mButtonBottom;
+    int mStoryIdTop=0;
+    Boolean mTopButtonPressed=null;
+    Boolean mBottomButtonPressed=null;
+    int mStoryIdBottom=0;
 
 
     @Override
@@ -13,18 +23,87 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         // TODO: Step 5 - Wire up the 3 views from the layout to the member variables:
+
+            mStoryTextView= findViewById(R.id.storyTextView);
+            mButtonTop= findViewById(R.id.buttonTop);
+            mButtonBottom= findViewById(R.id.buttonBottom);
 
 
 
         // TODO: Steps 6, 7, & 9 - Set a listener on the top button:
+        mButtonTop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mTopButtonPressed=true;
+                mBottomButtonPressed=null;
+                mStoryIdTop++;
+                upDateTopButton();
 
+            }
+
+        });
 
 
 
         // TODO: Steps 6, 7, & 9 - Set a listener on the bottom button:
 
+        mButtonBottom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mBottomButtonPressed =true;
+                mTopButtonPressed=null;
+                mStoryIdBottom++;
+                upDateBottomButton();
+            }
+        });
+    }
+    private void upDateTopButton(){
+        if(mStoryIdTop == 1 && mStoryIdBottom==0 && mTopButtonPressed){
+            mStoryTextView.setText(R.string.T3_Story);
+            mButtonTop.setText(R.string.T3_Ans1);
+            mButtonBottom.setText(R.string.T3_Ans2);
+        }
+        else if(mStoryIdTop == 2 && mStoryIdBottom==0 && mTopButtonPressed){
+            mStoryTextView.setText(R.string.T6_End);
+            mButtonTop.setVisibility(View.GONE);
+            mButtonBottom.setVisibility(View.GONE);
+        }
+        else if (mStoryIdBottom== 1 && mStoryIdTop== 1 && mTopButtonPressed){
+            mStoryTextView.setText(R.string.T3_Story);
+            mButtonTop.setText(R.string.T3_Ans1);
+            mButtonBottom.setText(R.string.T3_Ans2);
+        }
+        else if (mStoryIdBottom==1 && mStoryIdTop==2 && mTopButtonPressed){
+            mStoryTextView.setText(R.string.T6_End);
+            mButtonTop.setVisibility(View.GONE);
+            mButtonBottom.setVisibility(View.GONE);
+        }
 
     }
+    private void upDateBottomButton(){
+        if(mStoryIdBottom == 1 && mStoryIdTop==0 && mBottomButtonPressed){
+            mStoryTextView.setText(R.string.T2_Story);
+            mButtonTop.setText(R.string.T2_Ans1);
+            mButtonBottom.setText(R.string.T2_Ans2);
+        }
+        else if(mStoryIdBottom == 2 && mStoryIdTop==0 && mBottomButtonPressed){
+            mStoryTextView.setText(R.string.T4_End);
+            mButtonTop.setVisibility(View.GONE);
+            mButtonBottom.setVisibility(View.GONE);
+        }
+        else if (mStoryIdTop==1 && mStoryIdBottom==1 &&  mBottomButtonPressed){
+            mStoryTextView.setText(R.string.T5_End);
+            mButtonTop.setVisibility(View.GONE);
+            mButtonBottom.setVisibility(View.GONE);
+        }
+        else if (mStoryIdBottom==2 && mStoryIdTop==1 && mBottomButtonPressed){
+            mStoryTextView.setText(R.string.T5_End);
+            mButtonTop.setVisibility(View.GONE);
+            mButtonBottom.setVisibility(View.GONE);
+        }
+
+    }
+
+
 }
